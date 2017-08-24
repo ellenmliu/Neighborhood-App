0efaf3eb27d6a4585ac9abbb6269e6f836ca2f7f
Ellen Liu <ellen.m.liu@gmail.com>
2017-08-23 21:47:08 -0700

    Feat: Add markers and 5 points of interests
    These markers are default locations when the user first loads the page.
    This is to help the user see where the locations are relative on the map
    and later will add in more information and features to the areas.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,44 @@
 'use strict';
 var map;
+var markers = [];
+var locs = [
+  {
+    title: 'Exploratorium',
+    location: {lat: 37.800797, lng: -122.398868}
+  },
+  {
+    title: 'Union Square',
+    location: {lat:  37.788014, lng: -122.407477}
+  },
+  {
+    title: 'AT&T Park',
+    location:{lat: 37.779208, lng: -122.390157}
+  },
+  {
+    title: 'Golden Gate Park',
+    location: {lat: 37.769421, lng: -122.486214}
+  },
+  {
+    title: 'Walt Disney Museum',
+    location: {lat:  37.801339, lng: -122.458599}
+  }]
 
 var Map = function() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
+    center: {lat: 37.77493, lng: -122.419416},
     zoom: 13
+  });
+
+  locs.forEach(function(loc, index) {
+    var position = loc.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      animation: google.maps.Animation.Drop,
+      id: index
+    })
+
+    markers.push(marker);
   });
 }
 
